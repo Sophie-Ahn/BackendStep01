57927201b40025cf4835af1591a76b75c552ce16
Sophie-Ahn <sgdream95@gmail.com>
2024-04-02 11:08:14 +0900

    디스패치 구현방식

---
+++ src/main/java/_45_DispatchServlet/listeners/ContextLoaderListener.java
@@ -0,0 +1,65 @@
+package _45_DispatchServlet.listeners;
+
+import _45_DispatchServlet.dao.MemberDao;
+import org.apache.commons.dbcp.BasicDataSource;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/* 웹 어플리케이션이 실행되었을 때 자동으로 호출되는 클래스
+* ServletContext 영역이 준비되었습니다...
+*
+* @WebListener를 안쓰면 web.xml에 따로 작성을 해야함
+* */
+//@WebListener
+public class ContextLoaderListener implements ServletContextListener {
+//    Connection conn;
+//    DBConnectionPool connPool;
+    BasicDataSource ds;
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("contextInitialized 호출");
+        try {
+            ServletContext sc = sce.getServletContext();
+            ds = new BasicDataSource();
+            ds.setDriverClassName(sc.getInitParameter("driver"));
+            ds.setUrl(sc.getInitParameter("url"));
+            ds.setUsername(sc.getInitParameter("username"));
+            ds.setPassword(sc.getInitParameter("password"));
+
+            MemberDao memberDao = new MemberDao();
+            memberDao.setDataSource(ds);
+
+//            connPool = new DBConnectionPool(
+//                    sc.getInitParameter("driver"),
+//                    sc.getInitParameter("url"),
+//                    sc.getInitParameter("username"),
+//                    sc.getInitParameter("password")
+//            );
+//            MemberDao memberDao = new MemberDao();
+//            memberDao.setDBConnectionPool(connPool);
+
+            // connection객체대신 memberDao객체를 저장
+            sc.setAttribute("memberDao", memberDao);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("contextDestroyed 호출");
+        try{
+            if(ds != null) {
+                ds.close();
+            }
+//            connPool.closeAll();
+//            if(conn != null && conn.isClosed() == false){
+//                conn.close();
+//            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/_45_DispatchServlet/servlet/MemberAddServlet.java
@@ -0,0 +1,46 @@
+package _45_DispatchServlet.servlet;
+
+import _45_DispatchServlet.dao.MemberDao;
+import _45_DispatchServlet.vo.Member;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/memberDispatch/add")
+@SuppressWarnings("serial")
+public class MemberAddServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher(
+                "/member/MemberForm.jsp"
+        );
+        resp.setContentType("text/html;charset=UTF-8");
+        rd.include(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            ServletContext sc = this.getServletContext();
+            MemberDao memberDao = (MemberDao)sc.getAttribute("memberDao");
+            memberDao.insert(new Member()
+                    .setEmail(req.getParameter("email"))
+                    .setPassword(req.getParameter("password"))
+                    .setName(req.getParameter("name"))
+            );
+
+            resp.sendRedirect("list");
+        }catch (Exception e){
+            e.printStackTrace();
+            req.setAttribute("error", e);
+            RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
+            rd.forward(req, resp);
+        }
+    }
+}
